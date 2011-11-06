2c3a524b0f80fa34c44d61ab7d7bfb695ab0f186
Gordienok Maxim <aikismax@gmail.com>
2011-11-06 13:46:18 +0300

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseDesigning::Application.config.secret_token = 'ea5a486bc10a1298cbaf82a4eb00f993209c646a40d2b43cb5c9bec017163f9d6fa58fc0e5f71b363df88b83a40a87320f582c5d63590c0ccc500aab2c386822'
